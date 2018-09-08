3c5aaede70c9dab0bc4a7829f6e667043fa35505
ronquillojake <2015085176@ust-ics.mygbiz.com>
2018-09-08 09:03:23 +0800

    Initial commit

---
+++ app/src/main/java/jake/ronquillo/com/ronquillojakelab2/MainActivity.java
@@ -0,0 +1,13 @@
+package jake.ronquillo.com.ronquillojakelab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout);
+    }
+}
